d83c63c194bf8ac3ede63c5eced4ac47794b0c1a
minakamel <mina.kamel@mimi.io>
2025-02-16 18:04:18 +0200

    [Presentation] PhotosScreen

---
+++ photolist/presentation/src/main/java/com/helloclue/androidassignment/presentation/PhotosScreen.kt
@@ -0,0 +1,11 @@
+package com.helloclue.androidassignment.presentation
+
+import com.helloclue.androidassignment.presentation.addphoto.FloatingButton
+import androidx.compose.runtime.Composable
+import com.helloclue.androidassignment.presentation.grid.Photos
+
+@Composable
+fun PhotosScreen() {
+    Photos()
+    FloatingButton()
+}
